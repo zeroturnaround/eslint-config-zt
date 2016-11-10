e8b1b08c2c530fafffc3b29cd390b97d3218aab8
Uku Pattak <uku.pattak@zeroturnaround.com>
2016-11-10 16:01:29 +0200

    Remove recommended eslint rules extending and add rules that were extended before

--- index.js
+++ index.js
@@ -1,44 +1,124 @@
 const never = "never";
 const always = "always";
 
-const bestPractices = {
+const possibleErrorRules = {
+    "no-cond-assign": 2,
+
+    "no-console": 2,
+
+    "no-constant-condition": 2,
+
+    "no-control-regex": 2,
+
+    "no-debugger": 2,
+
+    "no-dupe-args": 2,
+
+    "no-dupe-keys": 2,
+
+    "no-duplicate-case": 2,
+
+    "no-empty": 2,
+
+    "no-empty-character-class": 2,
+
+    "no-ex-assign": 2,
+
+    "no-extra-boolean-cast": 2,
+
+    "no-extra-semi": 2,
+
+    "no-func-assign": 2,
+
+    "no-inner-declarations": 2,
+
+    "no-invalid-regexp": 2,
+
+    "no-irregular-whitespace": 2,
+
+    "no-obj-calls": 2,
+
+    "no-regex-spaces": 2,
+
+    "no-sparse-arrays": 2,
+
+    "no-unexpected-multiline": 2,
+
+    "no-unreachable": 2,
+
+    "no-unsafe-finally": 2,
+
+    "no-unsafe-negation": 2,
+
+    "use-isnan": 2,
+
+    "valid-typeof": 2,
+};
+
+const bestPracticeRules = {
     "curly": 2,
 
     "guard-for-in": 2,
 
+    "no-alert": 2,
+
+    "no-bitwise": 2,
+
+    "no-case-declarations": 2,
+
+    "no-empty-pattern": 2,
+
+    "no-fallthrough": 2,
+
+    "no-multi-spaces": 2,
+
+    "no-octal": 2,
+
+    "no-redeclare": 2,
+
+    "no-self-assign": 2,
+
+    "no-unused-labels": 2,
+
     "no-void": 2,
 
     "radix": 2,
+};
 
-    "no-alert": 2,
-
-    "eol-last": 2,
-
-    "no-bitwise": 2,
+const strictModeRules = {
+    "strict": 2,
 };
 
 const variablesRules = {
+    "no-delete-var": 2,
+
+    "no-undef": 2,
+
+    "no-unused-vars": 2,
+
     "no-use-before-define": [2, "nofunc"],
 };
 
 const styleRules = {
     "brace-style": [2, "stroustrup", { allowSingleLine: true }],
 
+    "eol-last": 2,
+
     "indent": [2, 4, { SwitchCase: 1 }],
+
+    "keyword-spacing": 2,
 
     "linebreak-style": [2, "unix"],
 
-    "space-infix-ops": 2,
+    "max-len": [2, 205, 4],
+
+    "no-mixed-spaces-and-tabs": 2,
 
     "no-multiple-empty-lines": [2, { max: 1 }],
 
     "one-var": [2, never],
 
-    "no-multi-spaces": 2,
-
     "operator-linebreak": [2, "after"],
-
-    "keyword-spacing": 2,
 
     "space-before-blocks": 2,
 
@@ -46,13 +126,25 @@ const styleRules = {
 
     "spaced-comment": 2,
 
-    "max-len": [2, 205, 4],
+    "space-infix-ops": 2,
 };
 
 const es6Rules = {
     "arrow-spacing": 2,
 
+    "constructor-super": 2,
+
+    "no-class-assign": 2,
+
+    "no-const-assign": 2,
+
+    "no-dupe-class-members": 2,
+
     "no-duplicate-imports": 2,
+
+    "no-new-symbol": 2,
+
+    "no-this-before-super": 2,
 
     "no-useless-constructor": 2,
 
@@ -64,11 +156,10 @@ const es6Rules = {
 
     "prefer-spread": 2,
 
-    "arrow-parens": [2, "as-needed"],
+    "require-yield": 2,
 };
 
 const reactRules = {
-
     "no-deprecated": 2,
 
     "no-direct-mutation-state": 2,
@@ -105,7 +196,7 @@ const reactRules = {
 
     "jsx-pascal-case": 2,
 
-    "jsx-max-props-per-line": [2, { maximum: 3 }],
+    "jsx-max-props-per-line": [2, { maximum: 5 }],
 
     "jsx-no-duplicate-props": 2,
 
@@ -119,10 +210,6 @@ const reactRules = {
 };
 
 module.exports = {
-    extends: [
-        "eslint:recommended"
-    ],
-
     env: {
         browser: true,
         node: true,
@@ -139,7 +226,9 @@ module.exports = {
 
     rules: Object.assign(
         {},
-        bestPractices,
+        possibleErrorRules,
+        bestPracticeRules,
+        strictModeRules,
         variablesRules,
         styleRules,
         es6Rules,
